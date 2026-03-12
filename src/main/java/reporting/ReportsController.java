7c37706757f7139614c0f6eb57d33a919fb3a6a2
Sylwia B <sylwia.barteczko95@gmail.com>
2026-03-11 21:46:59 -0700

    Kafka updated

--- src/main/java/reporting/ReportsController.java
+++ src/main/java/reporting/ReportsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 import java.util.TreeMap;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 @RestController
 @RequestMapping("/api/reports")
 public class ReportsController {
